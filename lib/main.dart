4de8d65a6a2134ef328e59a83a04aa1f5cc60e69
Wuletaw Wonte <wuletawwonte@yahoo.com>
2019-09-30 11:47:53 +0300

    language choice works

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,16 @@
 import "package:flutter/material.dart";
-import "package:flutter/services.dart";
 import 'package:gamo_dict/pages/home.dart';
 
 main() {
-  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
-    // systemNavigationBarColor: Colors.transparent, // navigation bar color
-    statusBarColor: Colors.orange, // status bar color
-  ));
   runApp(MaterialApp(
     title: "Gamo Dictionary",
     debugShowCheckedModeBanner: false,
+    // theme: ThemeData(
+    //   primarySwatch: Colors.orange,
+    //   ),
     home: HomeScreen(),
+
+  
   ));
 }
 
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:gamo_dict/CustomShapeClipper.dart';
+import "package:flutter/services.dart";
 
 class HomeScreen extends StatelessWidget {
 
   @override  
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    // systemNavigationBarColor: Colors.transparent, // navigation bar color
+    statusBarColor: Colors.orange, // status bar color
+  ));
     return Scaffold(
       appBar: AppBar(
         title: Text("Gamo-Dict"),
@@ -13,24 +18,22 @@ class HomeScreen extends StatelessWidget {
         actions: <Widget>[
           IconButton(icon: Icon(Icons.more_vert), onPressed: (){
             showSearch(context: context, delegate: WordSearch());
-          },)
+          },),
         ],
-
         ),
       drawer: Drawer(        
         child: ListView(
           children: <Widget>[
             DrawerHeader(
               decoration: BoxDecoration(
-                color: Colors.white,
+                color: Colors.orange,
               ),
-              child: Text("This is drawer header")              
+              child: Text("This is drawer header", style: TextStyle(color: Colors.white),)              
               ),
-            CustomListTile(Icons.translate, "Dictionary", ()=>{}),
-            CustomListTile(Icons.dashboard, "Dashboard", ()=>{}),
-            CustomListTile(Icons.favorite_border, "Favourite", ()=>{}),
-            CustomListTile(Icons.offline_bolt, "Bolt", ()=>{}),
-            CustomListTile(Icons.satellite, "Satelite", ()=>{}),
+            CustomListTile(Icons.home, "Home", ()=>{}),
+            CustomListTile(Icons.history, "History", ()=>{}),
+            CustomListTile(Icons.favorite_border, "Favourites", ()=>{}),
+            CustomListTile(Icons.calendar_today, "Word of the Month", ()=>{}),
             Container(
               decoration: BoxDecoration(
                 border: Border(top: BorderSide(color: Colors.grey.shade400))
@@ -54,19 +57,27 @@ class HomeScreen extends StatelessWidget {
   }
 }
 
-class HomeTopPart extends StatelessWidget {
+class HomeTopPart extends StatefulWidget {
+  @override
+  _HomeTopPartState createState() => _HomeTopPartState();
+}
+
+class _HomeTopPartState extends State<HomeTopPart> {
+  bool isSelected = true;
+
 
   @override
   Widget build(BuildContext context) {
     final height = MediaQuery.of(context).size.height;
     final width = MediaQuery.of(context).size.width;
+    var textStyle = TextStyle(color: Colors.white, fontSize: 24.0);
     return Stack(
       children: <Widget>[
         ClipPath(clipper: CustomShapeClipper(), child: Container(height: height * 0.4, width: width, color: Colors.orange,
         child: Column(
           children: <Widget>[
-            SizedBox(height: 50.0,),
-            Text("Search your Word", style: TextStyle(color: Colors.white, fontSize: 24.0),textAlign: TextAlign.center,),
+            SizedBox(height: 40.0,),
+            Text("Search Your Word", style: textStyle,textAlign: TextAlign.center,),
             SizedBox(height: 30.0,),
             Padding(
               padding: EdgeInsets.symmetric(horizontal: 32.0),
@@ -81,16 +92,77 @@ class HomeTopPart extends StatelessWidget {
                     suffixIcon: Material(
                       elevation: 2.0,
                       borderRadius: BorderRadius.all(Radius.circular(30.0)),
-                      child: Icon(Icons.search, color: Colors.grey,)
+                      child: Icon(Icons.search, color: Colors.grey, ),
+                      
                     ),
+                    hintText: 'Enter Search Word',
                   ),
                 ),
                 ),
+            ),
+            SizedBox(height: 20,),
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                InkWell(
+                  child: LangChoice("English", isSelected),
+                  onTap: () {
+                    setState(() {
+                      isSelected = true;                      
+                    });
+                  },
+                  ),
+                SizedBox(width: 25,),
+                InkWell(
+                  child: LangChoice("አማርኛ", !isSelected), 
+                  onTap: () {
+                    setState(() {
+                      isSelected = false;                      
+                    });
+                  },
+                  ),
+              ],
             )
           ],
         )
         ),)
       ],
+    );
+    
+  }
+}
+
+
+
+class LangChoice extends StatefulWidget {
+  final String lang;
+  final bool selected;
+
+  LangChoice(this.lang, this.selected);
+  @override
+  _LangChoiceState createState() => _LangChoiceState();
+}
+
+class _LangChoiceState extends State<LangChoice> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 18.0, vertical: 8.0),
+      decoration: widget.selected ? BoxDecoration(
+        color: Colors.white.withOpacity(0.15),
+        borderRadius: BorderRadius.all(Radius.circular(20.0))
+      ) : null,
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Text(widget.lang, style: TextStyle(color: Colors.white, fontSize: 18.0),),
+          SizedBox(
+            width: 4.0,
+          ),
+        ],
+      ),
     );
   }
 }
